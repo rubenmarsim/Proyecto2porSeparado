51ab62754cdb0b22631025104d1fa15b04e930ce
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-01-10 17:45:13 +0100

    zip ready

--- Proyecto2Main/Proyecto2Main/Proyecto2Main.cs
+++ Proyecto2Main/Proyecto2Main/Proyecto2Main.cs
@@ -6,12 +6,26 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
 using System.Windows.Forms;
 
 namespace Proyecto2Main
 {
     public partial class Proyecto2Main : Form
     {
+        #region Variables e instancias Globales
+
+        #region ZIP
+        const string _PathArchive = @"archivos\PruebaZIP.txt";
+        const string _PathZIP = @"archivos\PruebaZIP.zip";
+        const string _PathExtract = @"archivos\extract";
+        const string _Extension = ".zip";
+        FileInfo _FileInfo;
+        #endregion
+
+        #endregion
+
         /// <summary>
         /// Constructor por defecto
         /// </summary>
@@ -22,22 +36,47 @@ namespace Proyecto2Main
 
         private void Proyecto2Main_Load(object sender, EventArgs e)
         {
-            #region Variables e instancias Globales
-
-            #endregion
+            _FileInfo = new FileInfo(_PathArchive);
         }
 
 
         #region ZIP UNZIP
+
+        #region Events
         private void btnZIP_Click(object sender, EventArgs e)
         {
-
+            Compress(_FileInfo);
         }
 
         private void btnUNZIP_Click(object sender, EventArgs e)
         {
-
+            
         }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fi"></param>
+        private void Compress(FileInfo fi)
+        {
+            using (FileStream inFile = fi.OpenRead())
+            {
+                if((File.GetAttributes(fi.FullName)& FileAttributes.Hidden)!= FileAttributes.Hidden & fi.Extension != _Extension)
+                {
+                    using (FileStream outFile = File.Create(fi.FullName + _Extension))
+                    {
+                        using (GZipStream Compress = new GZipStream(outFile, CompressionMode.Compress))
+                        {
+                            inFile.CopyTo(Compress);
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
         #endregion
 
         #region Messages
